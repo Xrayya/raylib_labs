e433ee3b144e3dff66d55052d39b6319c516c3e5
Xrayya <nur.alkhiyar@gmail.com>
2026-06-27 17:43:33 +0700

    feat: add a non permanent work around of imgui scaling issue

--- src/app_core/application.cpp
+++ src/app_core/application.cpp
@@ -1,4 +1,5 @@
 #include "application.hpp"
+#include "imgui.h"
 #include "raylib.h"
 #include "rlImGui.h"
 
@@ -7,14 +8,21 @@
 namespace AppCore {
 
 Application::Application(ApplicationSpec spec) : m_spec(std::move(spec)) {
-  constexpr int screenWidth = 500;
-  constexpr int screenHeight = 500;
+  constexpr int screenWidth = 1280;
+  constexpr int screenHeight = 800;
 
   constexpr int fps = 60;
+
+  SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE);
 
   InitWindow(screenWidth, screenHeight, m_spec.name.c_str());
   SetTargetFPS(fps);
   rlImGuiSetup(true);
+
+  // TODO: make this configured form env var
+  constexpr float scale = 2.0F;
+
+  ImGui::GetIO().FontGlobalScale = scale;
 }
 
 auto Application::run() -> void {

a6a24bf2aa239771673d150eacfe04fc5d0ffd93
Xrayya <nur.alkhiyar@gmail.com>
2026-08-18 17:36:58 +0700

    make ilayer fully virtual

--- src/app_core/layer.hpp
+++ src/app_core/layer.hpp
@@ -3,10 +3,17 @@
 namespace AppCore {
 class ILayer {
 public:
-  auto onInit() -> void;
-  auto onDestroy() -> void;
-  auto onAttach() -> void;
-  auto onDetach() -> void;
-  auto onRender() -> void;
+  ILayer(const ILayer &) = default;
+  ILayer(ILayer &&) = delete;
+  auto operator=(const ILayer &) -> ILayer & = default;
+  auto operator=(ILayer &&) -> ILayer & = delete;
+
+  virtual ~ILayer() = default;
+
+  virtual auto onInit() -> void = 0;
+  virtual auto onDestroy() -> void = 0;
+  virtual auto onAttach() -> void = 0;
+  virtual auto onDetach() -> void = 0;
+  virtual auto onRender() -> void = 0;
 };
 } // namespace AppCore

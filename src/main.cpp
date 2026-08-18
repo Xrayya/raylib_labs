e4fed33d2a3436c5d24473a1365da25f4df8c3c9
Xrayya <nur.alkhiyar@gmail.com>
2026-08-18 18:34:33 +0700

    orchestrate everything in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
+#include "app_core/application.hpp"
+#include "app_core/layer.hpp"
+#include "layers/blue_layer.hpp"
+#include "layers/red_layer.hpp"
 #include <cstdio>
-#include <print>
+#include <memory>
+#include <utility>
 
 auto main() -> int {
-  std::println("Hello world");
+  constexpr int specWidth = 500;
+  constexpr int specHeight = 500;
+  constexpr int specFPS = 60;
+
+  AppCore::SApplicationSpec spec = {
+      .width = specWidth,
+      .height = specHeight,
+      .appName = "Arch Test",
+      .targetFPS = specFPS,
+  };
+
+  AppCore::CApplication app(spec);
+  std::unique_ptr<AppCore::ILayer> redLayer = std::make_unique<RedLayer>();
+  std::unique_ptr<AppCore::ILayer> blueLayer = std::make_unique<BlueLayer>();
+
+  app.pushLayer(std::move(redLayer));
+  app.pushLayer(std::move(blueLayer));
+
+  app.run();
 
   getchar();
   return 0;

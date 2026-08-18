95d39422f40faf1806170fe973b14b6ebadd1c0f
Xrayya <nur.alkhiyar@gmail.com>
2026-08-18 16:34:32 +0700

    cleanup

--- src/app_core/layer.cpp
+++
@@ -1,6 +0,0 @@
-#include "layer.hpp"
-#include "app_core/event.hpp"
-
-namespace AppCore {
-Layer::Layer(EventBus &eventBus) : m_eventBus(eventBus) {}
-} // namespace AppCore
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,9 @@
-#include "app_core/application.hpp"
-#include "layers/overlay_layer.hpp"
-#include "layers/red_layer.hpp"
 #include <cstdio>
+#include <print>
 
 auto main() -> int {
-  AppCore::ApplicationSpec appSpec = {.name = "Raylib Things"};
-
-  AppCore::Application app(appSpec);
-  app.pushLayer<RedLayer>();
-  app.pushLayer<OverlayLayer>();
-  app.run();
+  std::println("Hello world");
 
   getchar();
-
   return 0;
 }

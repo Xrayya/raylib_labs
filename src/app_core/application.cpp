03f0cc3004034855d50e073e735a35fe3b9c41a3
Xrayya <nur.alkhiyar@gmail.com>
2026-08-18 17:44:58 +0700

    add appspec and use const ref if possible

--- src/app_core/application.cpp
+++ src/app_core/application.cpp
@@ -1,16 +1,23 @@
 #include "app_core/application.hpp"
 #include "app_core/layer.hpp"
+#include "raylib.h"
 #include <cassert>
 #include <cstddef>
 #include <memory>
 #include <utility>
 
 namespace AppCore {
+CApplication::CApplication(const SApplicationSpec &spec) {
+  InitWindow(spec.width, spec.height, spec.appName.c_str());
+  SetTargetFPS(spec.targetFPS);
+}
+
 auto CApplication::pushLayer(std::unique_ptr<ILayer> nextLayer) -> void {
   m_layerStack.push_back(std::move(nextLayer));
 }
 
-auto CApplication::swapLayer(size_t layer1Idx, size_t layer2Idx) -> void {
+auto CApplication::swapLayer(const size_t &layer1Idx, const size_t &layer2Idx)
+    -> void {
   assert(layer1Idx <= m_layerStack.size() - 1 &&
          layer2Idx <= m_layerStack.size());
 
--- src/app_core/application.hpp
+++ src/app_core/application.hpp
@@ -3,13 +3,23 @@
 #include "app_core/layer.hpp"
 #include <cstddef>
 #include <memory>
+#include <string>
 #include <vector>
 
 namespace AppCore {
+struct SApplicationSpec {
+  int width;
+  int height;
+  std::string appName;
+  int targetFPS;
+};
+
 class CApplication {
 public:
+  CApplication(const SApplicationSpec &spec);
+
   auto pushLayer(std::unique_ptr<ILayer> nextLayer) -> void;
-  auto swapLayer(size_t layer1Idx, size_t layer2Idx) -> void;
+  auto swapLayer(const size_t &layer1Idx, const size_t &layer2Idx) -> void;
 
 private:
   std::vector<std::unique_ptr<ILayer>> m_layerStack;
